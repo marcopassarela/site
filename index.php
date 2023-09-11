47f312767bb5733e910c3978580684aa1dc88e78
marcopassarela <marcopassarela@hotmail.com>
2023-09-11 18:43:04 -0300

    Update index.html and index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// Conecte-se ao banco de dados (substitua com suas configurações)
+$servername = "localhost";
+$username = "root";
+$password = "Marco@978567";
+$dbname = "trabalhe_conosco";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifique a conexão
+if ($conn->connect_error) {
+    die("Conexão com o banco de dados falhou: " . $conn->connect_error);
+}
+
+// Receba os dados do formulário
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$dataNascimento = $_POST['data_nascimento'];
+
+// Insira os dados no banco de dados
+$sql = "INSERT INTO Pessoas (Nome, Telefone, Email, DataNascimento) VALUES ('$nome', '$telefone', '$email', '$dataNascimento')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados enviados com sucesso!";
+} else {
+    echo "Erro ao inserir dados: " . $conn->error;
+}
+
+// Feche a conexão com o banco de dados
+$conn->close();
+?>
