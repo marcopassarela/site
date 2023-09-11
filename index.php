0d77d28429291298bf7937b0c4f74bc2871b09cd
marcopassarela <marcopassarela@hotmail.com>
2023-09-11 19:02:15 -0300

    :rocket: Create database test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Conecte-se ao banco de dados (substitua com suas configurações)
 $servername = "localhost";
 $username = "root";
-$password = "Marco@978567";
+$password = "Marco@5213";
 $dbname = "trabalhe_conosco";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
