3569c422f0fccddf4870279f126499cbbe99679b
marcopassarela <marcopassarela@hotmail.com>
2023-09-11 20:37:49 -0300

    configurando Database

--- index.php
+++ index.php
@@ -12,21 +12,43 @@ if ($conn->connect_error) {
     die("Conexão com o banco de dados falhou: " . $conn->connect_error);
 }
 
-// Receba os dados do formulário
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
-$email = $_POST['email'];
-$dataNascimento = $_POST['data_nascimento'];
+// Verifique se a solicitação foi feita via método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Receba os dados do formulário
+    if (isset($_POST['nome'])) {
+        $nome = $_POST['nome'];
+    } else {
+        $nome = ''; // Ou outra ação apropriada se 'nome' não estiver definido
+    }
 
-// Insira os dados no banco de dados
-$sql = "INSERT INTO Pessoas (Nome, Telefone, Email, DataNascimento) VALUES ('$nome', '$telefone', '$email', '$dataNascimento')";
+    if (isset($_POST['telefone'])) {
+        $telefone = $_POST['telefone'];
+    } else {
+        $telefone = ''; // Ou outra ação apropriada se 'telefone' não estiver definido
+    }
 
-if ($conn->query($sql) === TRUE) {
-    echo "Dados enviados com sucesso!";
-} else {
-    echo "Erro ao inserir dados: " . $conn->error;
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+    } else {
+        $email = ''; // Ou outra ação apropriada se 'email' não estiver definido
+    }
+
+    if (isset($_POST['data_nascimento'])) {
+        $dataNascimento = $_POST['data_nascimento'];
+    } else {
+        $dataNascimento = ''; // Ou outra ação apropriada se 'data_nascimento' não estiver definido
+    }
+
+    // Insira os dados no banco de dados
+    $sql = "INSERT INTO pessoas (Nome, Telefone, Email, DataNascimento) VALUES ('$nome', '$telefone', '$email', '$dataNascimento')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Dados enviados com sucesso!";
+    } else {
+        echo "Erro ao inserir dados: " . $conn->error;
+    }
+
+    // Feche a conexão com o banco de dados
+    $conn->close();
 }
-
-// Feche a conexão com o banco de dados
-$conn->close();
 ?>
