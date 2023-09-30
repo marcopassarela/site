2cc36a4d230f3cc51a54d78c17474a3bdce86f29
marcopassarela <marcopassarela@hotmail.com>
2023-09-29 21:49:34 -0300

    Uptade PHP

--- seja aluno.php
+++ seja aluno.php
@@ -1,39 +1,31 @@
 <?php
-// Conexão com o banco de dados (substitua as informações com as suas)
-$servidor = "localhost";
-$usuario = "root";
-$senha = "1234";
-$banco = "seja_nosso_aluno";
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "seja_nosso_aluno";
 
-$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+// Cria conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
-if (!$conexao) {
-    die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+// Verifica conexão
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// Verifique se os campos do formulário foram enviados
-if (isset($_POST['nome']) && isset($_POST['telefone']) && isset($_POST['email']) && isset($_POST['pacote']) && isset($_POST['cursos'])) {
-    // Obtenha os valores dos campos
-    $nome = $_POST['nome'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $pacote = $_POST['pacote'];
-    $cursos = $_POST['cursos'];
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$pacote = $_POST['pacote'];
+$cursos = $_POST['cursos'];
 
-    // Inserir dados no banco de dados
-    $sql = "INSERT INTO formulario (nome, telefone, email, pacote, cursos)
-            VALUES ('$nome', '$telefone', '$email', '$pacote', '$cursos')";
+$sql = "INSERT INTO formulario (nome, telefone, email, pacote, cursos)
+VALUES ('$nome', '$telefone', '$email', '$pacote', '$cursos')";
 
-    if (mysqli_query($conexao, $sql)) {
-        echo "Dados inseridos com sucesso!";
-    } else {
-        echo "Erro ao inserir dados: " . mysqli_error($conexao);
-    }
+if ($conn->query($sql) === TRUE) {
+  echo "Novo registro criado com sucesso";
 } else {
-    echo "Campos do formulário não foram enviados corretamente.";
+  echo "Erro: " . $sql . "<br>" . $conn->error;
 }
 
-// Fechar a conexão com o banco de dados
-mysqli_close($conexao);
+$conn->close();
 ?>
